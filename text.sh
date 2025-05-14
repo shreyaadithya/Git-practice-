900e491a24794d067e089e8524c9ae838355a7da
shreyaadithya <shreyaadithya28@gmail.com>
2025-05-14 13:33:42 +0530

    checking for merge conflict

---
+++ text.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+read -p "enter input1" a
+read -p "enter input2" b
+read -p "chose + or /" c
+
+case $c in
+	"+") sum=$(($a + $b)) ;;
+	"/") div=$(($a /$b)) ;;
+	*)echo "please select between these 2";;
+esac
+echo "$sum $div"
